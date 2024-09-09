b4cc65de73bc47dc5b78f8ef665397c9c4bb7b3e
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2024-09-09 22:10:45 +0300

    init

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/mgmt/mcumgr/transport/smp_bt.h>
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
@@ -19,6 +21,59 @@
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN	(sizeof(DEVICE_NAME) - 1)
+
+
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
+
+static const struct bt_data sd[] = {
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL,
+		      0x84, 0xaa, 0x60, 0x74, 0x52, 0x8a, 0x8b, 0x86,
+		      0xd3, 0x4c, 0xb7, 0x1d, 0x1d, 0xdc, 0x53, 0x8d),
+};
+
+
+
+#define BT_LE_ADV_CONN_CUSTOM BT_LE_ADV_PARAM(BT_LE_ADV_OPT_CONNECTABLE, \
+				       BT_GAP_ADV_SLOW_INT_MIN, \
+				       BT_GAP_ADV_SLOW_INT_MAX, NULL)
+
+
+// static const bt_le_adv_param *ble_param_struct = BT_LE_ADV_CONN_CUSTOM;
+
+static int BLE_Start(void)
+{
+	int err = 0;
+
+
+	err = bt_enable(NULL);
+	if (err) {
+		//error();
+	}
+
+	
+
+	smp_bt_register();
+
+		
+	err = bt_le_adv_start(BT_LE_ADV_CONN_CUSTOM, ad, ARRAY_SIZE(ad), sd,
+			      ARRAY_SIZE(sd));
+
+
+	if (err) {
+		//LOG_ERR("Advertising failed to start (err %d)", err);
+		return -1;
+	}
+
+
+	return err;
+}
+
 void main(void)
 {
 	int ret;
@@ -31,6 +86,9 @@ void main(void)
 	if (ret < 0) {
 		return;
 	}
+
+	//start_smp_bluetooth_adverts();
+	BLE_Start();
 
 	while (1) {
 		ret = gpio_pin_toggle_dt(&led);
